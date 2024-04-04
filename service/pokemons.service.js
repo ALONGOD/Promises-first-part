69544d5ff2bf0f20f762f052743bb40e20f4830e
ALONGOD <alongoddard1@gmail.com>
2024-04-04 16:48:24 +0300

    Use jquery

--- service/pokemons.service.js
+++ service/pokemons.service.js
@@ -1,26 +1,8 @@
 'use strict'
 
-function getPokemonList(onSuccess) {
-    const xhr = new XMLHttpRequest()
-
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            const ans = JSON.parse(xhr.responseText)
-            onSuccess(ans)
-        }
-    }
-    xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon?limit=100', true)
-    xhr.send()
+function getPokemonList(onSuccess,pokemonLength) {
+    $.get(`https://pokeapi.co/api/v2/pokemon?limit=${pokemonLength}`, onSuccess)
 }
 function getPokemon(onSuccess, name) {
-    const xhr = new XMLHttpRequest()
-
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            const ans = JSON.parse(xhr.responseText)
-            onSuccess(ans)
-        }
-    }
-    xhr.open('GET', `https://pokeapi.co/api/v2/pokemon/${name}`, true)
-    xhr.send()
+    $.get(`https://pokeapi.co/api/v2/pokemon/${name}`, onSuccess)
 }
